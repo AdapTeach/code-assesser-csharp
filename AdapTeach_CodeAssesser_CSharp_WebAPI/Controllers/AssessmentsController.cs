4fc3b7e0cd2ac33158ab37a668d5fed4dcaac69b
arnau-d <terof_78@hotmail.com>
2014-12-17 17:24:20 +0100

    rework compilation check

--- AdapTeach_CodeAssesser_CSharp_WebAPI/Controllers/AssessmentsController.cs
+++ AdapTeach_CodeAssesser_CSharp_WebAPI/Controllers/AssessmentsController.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
 using System.Web.Http;
+using AdapTeach_CSharp_Code_Assesser_WebAPI.Engine;
 using AdapTeach_CSharp_Code_Assesser_WebAPI.Models;
-using Microsoft.CSharp;
 
 namespace AdapTeach_CSharp_Code_Assesser_WebAPI.Controllers
 {
@@ -16,92 +10,13 @@ namespace AdapTeach_CSharp_Code_Assesser_WebAPI.Controllers
     /// </summary>
     public class AssessmentsController : ApiController
     {
-
-        private static CompilerResults CompileCode(CompilationUnit cu)
-        {
-            CodeDomProvider cpd = new CSharpCodeProvider();
-            CompilerParameters cp = new CompilerParameters();
-            cp.ReferencedAssemblies.Add("System.dll");
-            //cp.ReferencedAssemblies.Add("ClassLibrary1.dll");
-            cp.GenerateExecutable = false;
-            cp.GenerateInMemory = true;
-            // Set the assembly file name to generate.
-            cp.OutputAssembly = cu.Name;
-            // Invoke compilation.
-            return cpd.CompileAssemblyFromSource(cp, cu.Code);
-        }
-
-        private static SubmissionResult GetErrors(CompilerResults cr)
-        {
-            var pass = false;
-            var errors = new List<string>();
-            SubmissionResult toReturn = new SubmissionResult { Pass = pass, CompilationErrors = errors, FailedTestMessage = new List<string>() };
-
-            if (cr.Errors != null)
-            {
-                if (cr.Errors.HasWarnings)
-                { pass = true; }
-                if (cr.Errors.HasErrors)
-                { pass = false; }
-                foreach (CompilerError error in cr.Errors)
-                {
-                    errors.Add(error.ErrorText);
-                }
-            }
-            else
-            {
-                Assembly dll = cr.CompiledAssembly;
-            }
-            return toReturn;
-        }
-
-        /// <summary>
-        /// Parse le code c# 
-        /// </summary>
-        /// <param name="cu"></param>
-        /// <returns></returns>
-        private static SubmissionResult CheckCompilationUnit(CompilationUnit cu)
-        {
-            return GetErrors(CompileCode(cu));
-        }
-
-        /// <summary>
-        /// Compile le code c# 
-        /// </summary>
-        /// <param name="cu"></param>
-        /// <returns></returns>
-        private static bool CompileCompilationUnit(CompilationUnit cu)
-        {
-            return false;
-        }
-
-
-        /// <summary>
-        /// Fait passer la validation de la submission
-        /// </summary>
-        /// <param name="sub">submission request deserialized</param>
-        private static SubmissionResult ProcessSubmission(Submission sub)
-        {
-            SubmissionResult toReturn = null;
-
-            var currentAss = sub.Assessment;
-
-            foreach (var cu in sub.SubmittedCompilationUnits)
-            {
-                toReturn = CheckCompilationUnit(cu);
-            }
-
-
-            return toReturn;
-        }
-
         /// <summary>
         /// Web API qui traite une submission
         /// </summary>
         /// <param name="sub">objet JSON passé en HTTP POST déserialisé en objet .net Submission</param>
         /// <returns>reponse http contenant un objet SubmissionResult serialisé en JSON</returns>
         [HttpPost]
-        public IHttpActionResult GetAssessment(Submission sub)
+        public IHttpActionResult GetAssessment([FromBody] Submission sub)
         {
             try
             {
@@ -110,7 +25,7 @@ namespace AdapTeach_CSharp_Code_Assesser_WebAPI.Controllers
                     //return NotFound();
                     return InternalServerError(new Exception("Error while deserializing JSON object Submission"));
                 }
-                return Ok(AssessmentsController.ProcessSubmission(sub));
+                return Ok(SubmissionVerifier.ProcessSubmission(sub));
             }
             catch (Exception e)
             {
---
+++ AdapTeach_CodeAssesser_CSharp_WebAPI/Engine/SubmissionVerifier.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using AdapTeach_CSharp_Code_Assesser_WebAPI.Models;
+using Microsoft.CSharp;
+
+namespace AdapTeach_CSharp_Code_Assesser_WebAPI.Engine
+{
+    public static class SubmissionVerifier
+    {
+        private const string SPACE = " ";
+        private const string LEFT_PARENTHESIS = "(";
+        private const string RIGHT_PARENTHESIS = "): ";
+        private const string LINE = "Line: ";
+        private const string COLUMN = ", Column: ";
+
+        private static CompilerResults CompileCode(CompilationUnit cu)
+        {
+            CodeDomProvider cpd = new CSharpCodeProvider();
+            CompilerParameters cp = new CompilerParameters();
+            //cp.ReferencedAssemblies.Add("System.dll");
+            //cp.ReferencedAssemblies.Add("ClassLibrary1.dll");
+            cp.GenerateExecutable = false;
+            cp.GenerateInMemory = true;
+            // Set the assembly file name to generate.
+            cp.OutputAssembly = cu.Name;
+            // Invoke compilation.
+            return cpd.CompileAssemblyFromSource(cp, cu.Code);
+        }
+
+        /// <summary>
+        /// Return a list of compilation errors (including warnings) from a <see cref="CompilerResults"/>
+        /// </summary>
+        /// <param name="cr">compilation results</param>
+        /// <returns>list of compilation errors and warnings</returns>
+        private static List<string> GetErrors(CompilerResults cr)
+        {
+            var errors = new List<string>();
+            foreach (CompilerError error in cr.Errors)
+            {
+                errors.Add(LEFT_PARENTHESIS + LINE + error.Line + COLUMN + error.Column + RIGHT_PARENTHESIS + error.ErrorText);
+            }
+            return errors;
+        }
+
+        /// <summary>
+        /// Fait passer la validation de la submission
+        /// </summary>
+        /// <param name="sub">submission request deserialized</param>
+        public static SubmissionResult ProcessSubmission(Submission sub)
+        {
+            SubmissionResult toReturn = new SubmissionResult();
+
+            try
+            {
+                var currentAss = sub.Assessment;
+                //var compiledUnits = new List<CompilerResults>();
+
+                var totalErrors = new List<string>();
+                var pass = true;
+                
+                foreach (var cu in sub.SubmittedCompilationUnits)
+                {
+                    var compilationResult = CompileCode(cu);
+                    //compiledUnits.Add(compilationResult);
+                    if (compilationResult.Errors != null)
+                    {
+                        totalErrors.AddRange(GetErrors(compilationResult));
+                        if (compilationResult.Errors.HasErrors)
+                        {
+                            pass = false;
+                            toReturn.CompilationErrors = totalErrors;
+                            break;
+                        }
+                    }
+                }
+                toReturn.Pass = pass;
+            }
+            catch (Exception e)
+            {
+                toReturn.ExceptionMessage = e.Message;
+            }
+
+            return toReturn;
+        }
+    }
+}
